21231123464a76dddf75ee7434514ff9bbfba116
mrthomas20121 <thomas77300@gmail.com>
2020-08-10 09:50:18 +0200

    fixes

--- index.js
+++ index.js
@@ -7,29 +7,33 @@ let mainWindow;
 app.on('ready', function() {
   
   mainWindow = new BrowserWindow({
-    name: 'Main windows',
-    width: 920,
+    name: 'Barbu',
+    width: 1280,
     height: 720,
-    icon: path.join(__dirname,'app/assets/app_logo.png'),
+    icon: path.join(__dirname,'app/icons/barbu.png'),
     nodeIntegration: false
-  });
-	
+  })
+  
+  // close the app when closing
   app.on('window-all-closed', function() {
     app.quit();
-  });
+  })
   app.on('closed', function() {
     app.quit();
-  });
+  })
 
+  // open file event
   app.on('open-file', function (event, file) {
-      var content = fs.readFileSync(file).toString();
-      mainWindow.webContents.send('file-opened', file, content);
+    var content = fs.readFileSync(file).toString()
+    mainWindow.webContents.send('file-opened', file, content)
   });
   
   // mainWindow.setMenu(null);
+
+  // load url
   mainWindow.loadURL(url.format({
     pathname: path.join(__dirname, 'app/index.html'),
     protocol: 'file:',
     slashes: true
-  }));
-});
+  }))
+})
